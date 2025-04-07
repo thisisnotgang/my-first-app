23eceb2996659ac62545fdf2479aeb10216e5bce
thisisnotgang <thisisnotgang@naver.com>
2025-04-07 16:17:40 +0900

    modified:   package.json 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-first-app/'  // GitHub 리포지토리 이름
 })
