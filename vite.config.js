d5dea64b3c753088356d6e2a308448b93b30a583
thisisnotgang <thisisnotgang@naver.com>
2025-04-07 15:53:11 +0900

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-first-app/' // GitHub 리포지토리 이름으로 설정
+  base: '/my-first-app/'  // GitHub 리포지토리 이름
 })
