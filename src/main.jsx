5679e01e0844e5bf9048f84b8607cab0f93959b9
thisisnotgang <thisisnotgang@naver.com>
2025-04-07 15:25:00 +0900

    modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<Layout />}>
           {/* INDEX 엘리먼트 : App.jsx */}
           <Route index element={<App />} /> 
-
           {/* PATH 엘리먼트 */}
           <Route path="projects" element={<Projects />} />
           <Route path="contact" element={<Contact />} />
